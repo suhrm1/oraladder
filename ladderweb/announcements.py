c86db838aee75742328e7055b120184a1a05fb13
Markus Suhr <markus@msuhr.net>
2022-10-01 00:10:10 +0200

    Announcements patch

---
+++ ladderweb/announcements.py
@@ -0,0 +1,18 @@
+import os.path
+from typing import Tuple
+
+import yaml
+
+
+def get_announcements(announcements_yaml_file: str) -> list[Tuple[str, str]]:
+    """Reads date/message tuples from a yaml file
+
+    Returns
+    -------
+    list[Tuple[str, str]]
+    """
+    if os.path.exists(announcements_yaml_file):
+        with open(announcements_yaml_file, 'r') as file:
+            yaml_content = yaml.safe_load(file)
+            return [(date, msg) for date, msg in yaml_content.items()]
+    return []
--- raglweb/announcements.py
+++
@@ -1,6 +0,0 @@
-def get_announcements():
-    return [
-        ("2022-09-27", "The official Red Alert Global League Season 13 map pack has been "
-                       "released. Check out the <a href=\"info\">information page</a> "
-                       "to download the maps.")
-    ]
