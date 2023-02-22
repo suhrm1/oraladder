5e81456ecff9b0e4a20bcc72754bc558c7f360e9
Markus Suhr <markus@msuhr.net>
2023-02-22 23:41:55 +0100

    Refactor ladder game server Dockerfile to work with AppImage base

--- .docker/ladder_server/entrypoint.sh
+++ .docker/ladder_server/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Apply patches to server settings
-python3 -c "from srvwrap_minimal import patch; patch()"
-echo "Server settings patched."
-
 # Read banned profile IDs from /home/openra/banned_profiles
 BANS_FILE="/home/openra/banned_profiles"
 BANS=$(python3 -c "from srvwrap_minimal import apply_bans; apply_bans(\"$BANS_FILE\")")
@@ -11,4 +7,4 @@ export ProfileIDBlacklist
 echo "Banned profiles: ${ProfileIDBlacklist:-}"
 
 echo "Starting OpenRA server."
-/home/openra/lib/openra/server.sh
+/home/openra/server.sh
--- .docker/ladder_server/server.sh
+++ .docker/ladder_server/server.sh
@@ -4,7 +4,6 @@
 # see https://github.com/OpenRA/OpenRA/wiki/Dedicated for details
 
 Name="${Name:-"Dedicated Server"}"
-Mod="${Mod:-"ra"}"
 ListenPort="${ListenPort:-"1234"}"
 AdvertiseOnline="${AdvertiseOnline:-"True"}"
 Password="${Password:-""}"
@@ -14,6 +13,9 @@ RequireAuthentication="${RequireAuthentication:-"False"}"
 ProfileIDBlacklist="${ProfileIDBlacklist:-""}"
 ProfileIDWhitelist="${ProfileIDWhitelist:-""}"
 
+# Allow or suppress downloading maps from OpenRA Resource Center
+QueryMapRepository=${QueryMapRepository:-"False"}
+
 EnableSingleplayer="${EnableSingleplayer:-"False"}"
 EnableSyncReports="${EnableSyncReports:-"False"}"
 EnableGeoIP="${EnableGeoIP:-"True"}"
@@ -21,27 +23,25 @@ ShareAnonymizedIPs="${ShareAnonymizedIPs:-"True"}"
 
 SupportDir="${SupportDir:-""}"
 
-# Run loop to restart game server after a game has completed
-while true; do
-  # We rotate maps from the map folder to start with a random pick after each game
-  hash=$(shuf -n1 -e /home/openra/lib/openra/mods/${Mod}/maps/*.oramap)
-  Map=$(./utility.sh $Mod --map-hash $hash)
+# We rotate maps from the map folder to start with a random pick after each game
+hash=$(shuf -n1 -e /home/openra/usr/lib/openra/mods/${MOD}/maps/*.oramap)
+Map=$(/home/openra/AppRun --utility --map-hash $hash)
 
-  # Start the game server
-  mono --debug bin/OpenRA.Server.exe Engine.EngineDir=".." Game.Mod="$Mod" \
-    Server.Name="$Name" \
-    Server.ListenPort="$ListenPort" \
-    Server.AdvertiseOnline="$AdvertiseOnline" \
-    Server.EnableSingleplayer="$EnableSingleplayer" \
-    Server.Password="$Password" \
-    Server.RecordReplays="$RecordReplays" \
-    Server.GeoIPDatabase="$GeoIPDatabase" \
-    Server.RequireAuthentication="$RequireAuthentication" \
-    Server.ProfileIDBlacklist="$ProfileIDBlacklist" \
-    Server.ProfileIDWhitelist="$ProfileIDWhitelist" \
-    Server.EnableSyncReports="$EnableSyncReports" \
-    Server.EnableGeoIP="$EnableGeoIP" \
-    Server.ShareAnonymizedIPs="$ShareAnonymizedIPs" \
-    Server.Map="$Map" \
-    Engine.SupportDir="$SupportDir"
-done
+# Start the game server
+/home/openra/AppRun --server \
+  Server.Name="$Name" \
+  Server.ListenPort="$ListenPort" \
+  Server.AdvertiseOnline="$AdvertiseOnline" \
+  Server.QueryMapRepository="$QueryMapRepository" \
+  Server.EnableSingleplayer="$EnableSingleplayer" \
+  Server.Password="$Password" \
+  Server.RecordReplays="$RecordReplays" \
+  Server.GeoIPDatabase="$GeoIPDatabase" \
+  Server.RequireAuthentication="$RequireAuthentication" \
+  Server.ProfileIDBlacklist="$ProfileIDBlacklist" \
+  Server.ProfileIDWhitelist="$ProfileIDWhitelist" \
+  Server.EnableSyncReports="$EnableSyncReports" \
+  Server.EnableGeoIP="$EnableGeoIP" \
+  Server.ShareAnonymizedIPs="$ShareAnonymizedIPs" \
+  Server.Map="$Map" \
+  Engine.SupportDir="$SupportDir"
