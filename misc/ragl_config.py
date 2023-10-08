5f754e3e2052a95a627b833ebcdba8baad0df9c7
Markus Suhr <markus.suhr@nextlytics.com>
2023-10-08 11:27:53 +0200

    RAGL S15 config

--- misc/ragl_config.py
+++ misc/ragl_config.py
@@ -3,17 +3,17 @@ from datetime import date, timedelta
 LEAGUE_TITLE = "Red Alert Global League"
 LEAGUE_TITLE_SHORT = "RAGL"
 TITLE_IMAGE = "ragl-logo.png"
-SEASON = 14
-START_TIME = date(2023, 5, 1)
+SEASON = 15
+START_TIME = date(2023, 10, 9)
 GROUP_STAGE_WEEKS = 7
 PLAYOFF_WEEKS = 3
 DURATION_WEEKS = GROUP_STAGE_WEEKS + PLAYOFF_WEEKS
 END_TIME = START_TIME + timedelta(weeks=DURATION_WEEKS)
-MAP_PACK_VERSION = "2023-04-26"
-RELEASE = "release-20230225"
-RELEASE_URL = "https://github.com/OpenRA/OpenRA/releases/tag/release-20230225"
-SCHEDULE_URL = "https://forum.openra.net/viewtopic.php?f=85&t=21750"
-RULES_URL = "https://forum.openra.net/viewtopic.php?f=85&t=21735"
+MAP_PACK_VERSION = "2023-09-30"
+RELEASE = "playtest-20230927"
+RELEASE_URL = "https://github.com/OpenRA/OpenRA/releases/tag/" + RELEASE
+SCHEDULE_URL = "https://forum.openra.net/viewtopic.php?f=85&t=21802"
+RULES_URL = "https://forum.openra.net/viewtopic.php?f=85&t=21790"
 PRIZE_POOL = "TBA"
 DISCORD_URL = "https://discord.gg/99zBDuS"
 DISCORD_NAME = "Red Alert Competitive Discord"
