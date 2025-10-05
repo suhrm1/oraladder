78ffb44f7f0e87db761ec6e5ab0f04372b1824ab
Markus Suhr <markus@msuhr.net>
2025-10-05 12:34:29 +0200

    RAGL: S17 config

--- misc/ragl_config.py
+++ misc/ragl_config.py
@@ -3,22 +3,22 @@ from datetime import date, timedelta
 LEAGUE_TITLE = "Red Alert Global League"
 LEAGUE_TITLE_SHORT = "RAGL"
 TITLE_IMAGE = "ragl-logo.png"
-SEASON = 16
-START_TIME = date(2024, 10, 7)
+SEASON = 17
+START_TIME = date(2025, 10, 6)
 GROUP_STAGE_WEEKS = 7
 PLAYOFF_WEEKS = 3
 DURATION_WEEKS = GROUP_STAGE_WEEKS + PLAYOFF_WEEKS
 END_TIME = START_TIME + timedelta(weeks=DURATION_WEEKS)
-MAP_PACK_VERSION = "2024-09-29"
-RELEASE = "release-20231010"
+MAP_PACK_VERSION = "2025-09-27"
+RELEASE = "release-20250330"
 RELEASE_URL = "https://github.com/OpenRA/OpenRA/releases/tag/" + RELEASE
-SCHEDULE_URL = "https://forum.openra.net/viewtopic.php?f=85&t=21935"
-RULES_URL = "https://forum.openra.net/viewtopic.php?f=85&t=21924"
-PRIZE_POOL = "TBA"
+SCHEDULE_URL = "https://forum.openra.net/viewtopic.php?f=85&t=22104"
+RULES_URL = "https://forum.openra.net/viewtopic.php?f=85&t=22080"
+PRIZE_POOL = ">80 EUR"
 DISCORD_URL = "https://discord.gg/99zBDuS"
 DISCORD_NAME = "Red Alert Competitive Discord"
 GAMES_PER_MATCH = 2
-GAME_SERVER_NAMING_PATTERN = "RAGL Official Server"
+GAME_SERVER_NAMING_PATTERN = "[RAGL.org] Red Alert Global League Server"
 # PLAYOFF_SCHEMA_IMG = "tdgl_format_s03.png"
 
 
